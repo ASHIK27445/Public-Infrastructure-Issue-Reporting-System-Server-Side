194c6eb6e6f34b7c48e27109e697aadfc748b4ae
ASHIK27445 <mdashikulislama57@gmail.com>
2025-12-22 21:12:58 +0600

    keyConverter added and added a middleware

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const admin = require("firebase-admin")
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.md2layq.mongodb.net/?appName=Cluster0`;
 
